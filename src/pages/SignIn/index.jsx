de3f06140e6ecadf778592656d0b7b90c246bca0
odailsonsilva <odafreelancerti@gmail.com>
2020-11-12 16:44:43 -0300

    feat: estrutura inicial

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styled';
+
+const SignIn = () => {
+
+    return(
+        <Container>
+            <h1>SignIn</h1>
+        </Container>
+    );
+}
+
+export default SignIn;
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styled';
+
+const SignIn = () => {
+
+    return(
+        <Container>
+            <h1>SignUp</h1>
+        </Container>
+    );
+}
+
+export default SignIn;
